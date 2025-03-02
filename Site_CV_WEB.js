2aaa1944bfb64ef422ced6b36805150b847be07c
Lifdas <florian.potier-clemente@e-cdp.com>
2025-03-02 19:25:06 +0100

    <feat> Mise à jour pour 2025 session de candidatures (première ébauche)

--- Site_CV_WEB.js
+++ Site_CV_WEB.js
@@ -35,25 +35,22 @@ menuItems.forEach(item => {
         // Utilisez un switch pour déterminer le contenu à afficher en fonction de l'ID du contenu
         switch (contentId) {
             case 'content-js':
-                newContent = "<h3 class='language-title'>Langage JavaScript</h3><p> Le Menu de ce site web est codé en Javascript, je sais également créer du HTML et CSS, utiliser Ajax (pour du GET & du POST), fetch API (GET, POST, UPDATE, DELETE) et créer des fonctions dynamiques.";
+                newContent = "<h3 class='language-title'>Langage JavaScript</h3><p> Le Menu de ce site web est codé en Javascript, <br>Ajax / fetch API sont devenus une routine. <br> l'ERP de Ferbateq l'utilise à l'aide d'alpine.Js un mini-vue.js like.";
                 break;
             case 'content-en':
-                newContent = "<h3 class='language-title'>Niveau d'anglais</h3>Anglais B2 pratiqué quotidiennement pour de l'écrit et de l'écoute. How you doing ?";
+                newContent = "<h3 class='language-title'>Niveau d'anglais</h3>Anglais B2 (860 points au TOEIC)pratiqué quotidiennement pour de l'écrit et de l'écoute. How you doing ?";
                 break;
             case 'content-py':
-                newContent = "<h3 class='language-title'>Langage Python</h3>J'ai codé un casse briques en POO en première année et un small model language (type chat gpt) en seconde année.";
+                newContent = "<h3 class='language-title'>Langage Python</h3>J'ai codé un casse briques en POO en première année et un small model language (type chat gpt) en seconde année. <br> au cours de la troisième il m'a servi pour confectionner le back end de l'ERP de Ferbateq.";
                 break;
             case 'content-sql':
-                newContent = "<h3 class='language-title'>La base de données</h3>Mon domaine favori, je sais modéliser une BDD élaborée, création, insertion des données, et je maitrise les requêtes basiques et quelques une avancées.";
-                break;
-            case 'content-java':
-                newContent = "<h3 class='language-title'>Langage Java</h3>J'ai eu l'opportunité de faire de la POO en Java gérant entrées et sorties pour une Médiathéque simulée, ainsi que le développement de tests unitaires (Test Driven Development).";
+                newContent = "<h3 class='language-title'>La base de données</h3>Modéliser une BDD élaborée, création, insertion des données est devenu courant dans mon travail, je maitrise les requêtes basiques et quelques unes avancées. (HEIDI SQL / PhpMyAdmin)";
                 break;
             case 'content-php':
                 newContent = "<h3 class='language-title'>Langage PHP</h3>J'ai réalisé une architecture 4 tiers (couches: présentation, applicative, métiers, accès aux données) à partir de la base que mon formateur a pu mettre au point suite à 10 ans d'expérience, cette base est réutilisable et modulable pour l'adapter à d'autres projets. J'ai également créé de zéro une calculatrice envoyant toutes les opérations en base de données grâce à Ajax, ainsi que des requêtes pour sortir un historique sur la page front où se trouve ma calculatrice.";
                 break;
             case 'content-html-css':
-                newContent = "<h3 class='language-title'>Le HTML & CSS</h3>70 % de ce CV WEB est fait à partir de HTML et CSS, j'ai également eu l'occasion d'apprendre à m'en servir en créant un site vitrine.";
+                newContent = "<h3 class='language-title'>Le HTML & CSS</h3>70 % de ce CV WEB est fait à partir de HTML et CSS, j'ai également eu l'occasion d'apprendre à m'en servir en créant un site vitrine. <br> et la totalité du front de l'ERP de Ferbateq est en Html/css (mais avec Fomantic en Framework)";
                 break;
             // Ajoutez d'autres cas au besoin
             default:
